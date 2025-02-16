11c5a56849160d63d912565a3b799874f2568339
JZylber <zylberjulian@gmail.com>
2025-02-16 17:39:36 -0300

    feat: programs

--- src/components/init/global.d.ts
+++ src/components/init/global.d.ts
@@ -1,8 +1,6 @@
-import { Alpine as AlpineType } from "alpinejs";
 import { Course, Material, UnitData } from "./types";
 
 declare global {
-  var Alpine: AlpineType;
   var getSubjectData: (subject: string, course: string) => Promise<UnitData[]>;
   var getSubjectProgram: (subject: string) => Promise<string>;
   var getSubjectPresentation: (subject: string) => Promise<string>;
@@ -10,6 +8,4 @@ declare global {
     subject: string
   ) => Promise<{ activitiesRedo: string; markedActivitiesRedo: string }>;
   var getSubjectMaterial: (subject: string) => Promise<Material[]>;
-  var cloneTemplate: (id: string) => HTMLElement | undefined;
-  var Prism: any;
 }
