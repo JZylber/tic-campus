af42072bb3fb67e904275a40791892455e92db2f
JZylber <zylberjulian@gmail.com>
2025-11-13 09:50:44 -0300

    reafactor(home): data

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,14 +1,4 @@
 import { type Alpine } from "alpinejs";
-import {
-  getSubjectProgram,
-  getSubjectPresentation,
-  getSubjectRedoLinks,
-  getSubjectMaterial,
-  getCourseGroupLink,
-  getFixedMarks,
-  getStudents,
-} from "./scripts/fetchData";
-import { fetchHTMLData, prepareCrumbs } from "./scripts/loadData";
 import collapse from "@alpinejs/collapse";
 import persist from "@alpinejs/persist";
 import twMQDirective from "./scripts/alpine/directives/twMediaQuery";
@@ -16,16 +6,6 @@ import swipeDirective from "./scripts/alpine/directives/swipe";
 import pageData from "./scripts/alpine/stores/pageData";
 import courseStore from "./scripts/alpine/stores/course";
 import studentStore from "./scripts/alpine/stores/student";
-
-window.getSubjectProgram = getSubjectProgram;
-window.getSubjectPresentation = getSubjectPresentation;
-window.getSubjectRedoLinks = getSubjectRedoLinks;
-window.getSubjectMaterial = getSubjectMaterial;
-window.getCourseLink = getCourseGroupLink;
-window.fetchHTMLData = fetchHTMLData;
-window.prepareCrumbs = prepareCrumbs;
-window.getFixedMarks = getFixedMarks;
-window.getStudents = getStudents;
 
 export default (Alpine: Alpine) => {
   Alpine.plugin(collapse);
--- src/scripts/global.d.ts
+++ src/scripts/global.d.ts
@@ -3,42 +3,4 @@ import { Alpine as AlpineType } from "alpinejs";
 
 declare global {
   var Alpine: AlpineType;
-  var getSubjectProgram: (
-    subject: string,
-    dataSheetId: string
-  ) => Promise<string>;
-  var getSubjectPresentation: (
-    subject: string,
-    dataSheetId: string
-  ) => Promise<string>;
-  var getSubjectRedoLinks: (
-    subject: string,
-    dataSheetId: string
-  ) => Promise<{ activitiesRedo: string; markedActivitiesRedo: string }>;
-  var getSubjectMaterial: (
-    subject: string,
-    dataSheetId: string
-  ) => Promise<Material[]>;
-  var getCourseLink: (
-    course: string,
-    dataSheetId: string,
-    subject: string | null
-  ) => Promise<string>;
-  var fetchHTMLData: (url: string) => Promise<string>;
-  var prepareCrumbs: () => {
-    crumbs: Array<{ text: string; link: string }>;
-    menu: Array<string>;
-  };
-  var getFixedMarks: (
-    studentId: number,
-    dataSheetId: string
-  ) => Promise<FixedMarks>;
-  var getStudents: (dataSheetId: string) => Promise<
-    Array<{
-      DNI: number;
-      name: string;
-      surname: string;
-      course: string;
-    }>
-  >;
 }
