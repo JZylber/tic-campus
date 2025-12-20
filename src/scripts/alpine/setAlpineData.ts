0d3f6d3a31291eae6a61b8b1f013b665b70bc3ae
JZylber <zylberjulian@gmail.com>
2025-12-20 12:40:40 -0300

    fix(campus-insertion): set alpine data

--- src/scripts/alpine/setAlpineData.ts
+++ src/scripts/alpine/setAlpineData.ts
@@ -2,11 +2,11 @@ import type { AlpineComponent } from "alpinejs";
 
 const setAlpineData = (name: string, component: AlpineComponent<any>) => {
   // If Alpine has initialized, set the data immediately, else, wrap in addEventListener
-  if (Alpine) {
-    Alpine.data(name, component);
+  if (window.Alpine) {
+    window.Alpine.data(name, component);
   } else {
     document.addEventListener("alpine:init", () => {
-      Alpine.data(name, component);
+      window.Alpine.data(name, component);
     });
   }
 };
