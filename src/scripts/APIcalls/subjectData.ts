72e7243b65e1dd9f1b8af3ca52d5b82a5e10af31
JZylber <zylberjulian@gmail.com>
2025-12-26 12:47:48 -0300

    feat(API): home links

--- src/scripts/APIcalls/subjectData.ts
+++ src/scripts/APIcalls/subjectData.ts
@@ -1,16 +1,18 @@
 import type { Unit } from "../types";
 
-const backendURL = "http://localhost:3000";
+const backendURL = import.meta.env.DEV
+  ? "http://localhost:3000"
+  : "https://tic-campus-backend.vercel.app";
 
 export async function fetchSubjectData(
-  subjectName: string,
+  subject: string,
   course: string,
   year: number
 ): Promise<Unit[]> {
   try {
     const response = await fetch(
       `${backendURL}/articles/${encodeURIComponent(
-        subjectName
+        subject
       )}/${encodeURIComponent(course)}/${year}`
     );
     if (!response.ok) {
@@ -21,5 +23,30 @@ export async function fetchSubjectData(
   } catch (error) {
     console.error("Failed to fetch subject data:", error);
     return [];
+  }
+}
+
+export async function fetchHomeLinks(
+  subject: string,
+  course: string,
+  year: number
+): Promise<{
+  group: string;
+  presentation: string;
+}> {
+  try {
+    const response = await fetch(
+      `${backendURL}/homeLinks/${encodeURIComponent(
+        subject
+      )}/${encodeURIComponent(course)}/${year}`
+    );
+    if (!response.ok) {
+      throw new Error(`Error fetching home links: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch home links:", error);
+    return { group: "", presentation: "" };
   }
 }
