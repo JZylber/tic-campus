3bb599cf5f2943fcc5fc9535f916150ad06973ba
JZylber <zylberjulian@gmail.com>
2026-03-06 19:35:49 -0300

    refactor(redo): form data

---
+++ src/scripts/alpine/data/redos.ts
@@ -0,0 +1,43 @@
+import type { AlpineComponent } from "alpinejs";
+
+type Student = {
+  name: string;
+  surname: string;
+  id: string;
+  course: string;
+  year: number;
+};
+
+type AlpineRedoData = AlpineComponent<{
+  students: Array<Student>;
+  studentIds: Array<string>;
+  activityId: string;
+  reason: string;
+  bonusTask: string;
+  comment: string;
+  remainingStudents: Array<Student>;
+  selectedStudents: Array<Student>;
+}>;
+
+const redoData = (students: Array<Student>) => {
+  return {
+    students: students,
+    studentIds: [] as Array<string>,
+    activityId: "",
+    reason: "",
+    bonusTask: "",
+    comment: "",
+    get selectedStudents() {
+      return this.students.filter((student) =>
+        this.studentIds.includes(student.id),
+      );
+    },
+    get remainingStudents() {
+      return this.students.filter(
+        (student) => !this.studentIds.includes(student.id),
+      );
+    },
+  } as AlpineRedoData;
+};
+
+export default redoData;
